a828934bb245aa977f6d407c3846fd49a8a88cd4
Ahmet Balaman <56163828+ahmetbalaman@users.noreply.github.com>
2023-10-16 15:33:40 +0300

    v

---
+++ Lecture_5/Infrastructure/Persistance/Contexts/Productify.DbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+using Productify.Domain.Entities;
+
+namespace Productify.Persistance.Contexts
+{
+	public class ProductifyDbContext: DbContext 
+	{
+		public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+			optionsBuilder.UseInMemoryDatabase("ProductifyDb");
+
+        }
+
+        public ProductifyDbContext()
+		{
+		}
+	}
+}
+
---
+++ Lecture_5/Presentation/Productify.MVC/Controllers/ProductController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Productify.Domain.Entities;
+using Productify.Persistance.Contexts;
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Productify.MVC.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        //        List<Product>_products = new()
+        //   {
+        //       new Product("Laptop Computer"),
+        //       new Product("Smartphone"),
+        //       new Product("Wireless Headphones"),
+        //       new Product("Digital Camera"),
+        //       new Product("Tablet")
+        //   };
+
+
+        ProductifyDbContext _context;
+
+        public ProductController()
+        {
+            _context = new ProductifyDbContext();
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return View(_context.Products.ToList());
+        }
+
+       
+        [HttpPost]
+        public IActionResult GetAll(string ProductName)
+        {
+            _context.Products.Add(new Product(ProductName));
+            _context.SaveChanges();
+            return View(_context.Products.ToList());
+        }
+
+        [HttpPost]
+        public IActionResult DeleteProduct(Guid id)
+        {
+
+            var product = _context.Products.FirstOrDefault(p => p.Id == id);
+            if (product != null)
+            {
+                _context.Products.Remove(product);
+                _context.SaveChanges();
+            }
+
+            return RedirectToAction("GetAll");
+        }
+
+    }
+}
+
